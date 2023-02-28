8688a5e3ec72da2ca687990c3d8082f0bd72f0f5
marisvenancio <marisvenancio@gmail.com>
2023-02-28 14:49:45 -0600

    Fecthed Repo Data

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 // Selects the div where the profile information will appear
 const overview = document.querySelector(".overview");
 const username = "marisvenancio";
+const repoList = document.querySelector(".repo-list");
 
 const getUserData = async function () {
-    const request = await fetch(`https://api.github.com/users/${username}`);
-    const data = await request.json();
+    const requestData = await fetch(`https://api.github.com/users/${username}`);
+    const data = await requestData.json();
     console.log(data);
     displayInfo(data);
 };
@@ -25,5 +26,24 @@ const displayInfo = function(data) {
         <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
     </div>`
     overview.append(div);
+    getRepos();
+};
+
+const getRepos = async function () {
+    const fetchRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repoData = await fetchRepos.json();
+    // console.log(repoData);
+    displayRepos(repoData);
+};
+
+// getRepos();
+
+const displayRepos = function(repos) {
+    for (let repo of repos) {
+        const repoItem = document.createElement("li");
+        repoItem.classList.add("repo");
+        repoItem.innerHTML = `<h3>${repo.name}</h3>`
+        repoList.append(repoItem);
+    }
 };
 
